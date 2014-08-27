c1384c212f90030216172bc2deb42420ee6ed09b
ccrom1 <legocrazy101@gmail.com>
2014-08-26 19:15:34 -0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,61 @@
+<?php
+/* WebLightshowpi config file ;)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+YOU MUST NOT HAVE THE LIGHTSHOWPI DIRECTORY IN THE ROOT DIRECTORY!
+Place the directory in the top level of the file tree!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+Just fill out the info below to get started!
+TIP: If you have your music in the lightshowpi directory and in folders by artists, dont use spaces to avoid possible problems.
+In the line that says $homepage put the ip address of the pi. If you have the Weblightshowpi downloaded to somewhere other than /var/www/ then ask the google plus community.
+In the line that says $lightshowpihome , put the *full* location of the lightshowpi directory in quotes!
+Where the file says song1, song2, etc etc, put the name of the song in quotes. Capitals and spaces.
+Where the file says location1, location2 etc etc, put the full location of the song in quotes. 
+So example,
+$lightshowpihome = "/lightshowpi"
+$song1 = "Deck the halls";
+$location1 = "/lights/music/sample/ovenrake_deck-the-halls.mp3";
+
+If you need help, just join the google plus community at https://plus.google.com/u/1/communities/101789596301454731630 and ask away!
+*/
+$homepage = "192.168.0.109";
+$lightshowpihome = "/lights";
+$song1 = "Deck the Halls";
+$location1 = "/lights/music/sample/ovenrake_deck-the-halls.mp3";
+$song2 = "saads";
+$location2 = "asdfsdf";
+$song3 = "asdfasdf";
+$location3 = "sdfasdf";
+$song4 = "sdfasdf";
+$location4 = "sdfasdf";
+$song5 = "sdfasdf";
+$location5 = "sdfasdf";
+$song6 = "sdfasdf";
+$location6 = "sdfasdf";
+$song7 = "sdfasdf";
+$location7 = "sdfasdf";
+$song8 = "sdfasdf";
+$location8 = "sdfasdf";
+$song9 = "sdfasdf";
+$location9 = "sdfasdf";
+$song10 = "sdfasdf";
+$location10 = "sdfasdf";
+$song11 = "sdfasdf";
+$location11 = "sdfasdf";
+$song12 = "sdfasdf";
+$location12 = "sdfasdf";
+$song13 = "sdfasdf";
+$location13 = "sdfasdf";
+$song14 = "sdfasdf";
+$location14 = "sdfasdf";
+$song15 = "sdfasdf";
+$location15 = "sdfasdf";
+$song16 = "sdfasdf";
+$location16 = "sdfasdf";
+$song17 = "sdfasdf";
+$location17 = "sdfasdf";
+$song18 = "sdfasdf";
+$location18 = "sdfasdf";
+$song19 = "sdfasdf";
+$location19 = "sdfasdf";
+$song20 = "sdfasdf";
+$location20 = "sdfasdf";
