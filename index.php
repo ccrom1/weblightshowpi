59edcbaa5181ef011104a0cf92aa7b4debc0d387
ccrom1 <legocrazy101@gmail.com>
2014-08-26 19:14:48 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+include '/var/www/lightshowpi/config.php';
+echo "<html><body><h1>Welcome to the Lightshowpi Controller window!</h1>";
+if ($location1 != "") {
+echo "<a href='play.php?location=".$location1."'>Play ".$song1. "</a>";
+};
+echo "</br>";
+if ($location2 != "") {
+echo "<a href='play.php?location=".$location2."'>Play ".$song2. "</a>";
+};
+echo "</br>";
+if ($location3 != "") {
+echo "<a href='play.php?location=".$location3."'>Play ".$song3. "</a>";
+};
+echo "</br>";
+if ($location4 != "") {
+echo "<a href='play.php?location=".$location4."'>Play ".$song4. "</a>";
+};
+echo "</br>";
+if ($location5 != "") {
+echo "<a href='play.php?location=".$location5."'>Play ".$song5. "</a>";
+};
+echo "</br>";
+if ($location6 != "") {
+echo "<a href='play.php?location=".$location6."'>Play ".$song6. "</a>";
+};
+echo "</br>";
+if ($location7 != "") {
+echo "<a href='play.php?location=".$location7."'>Play ".$song7. "</a>";
+};
+echo "</br>";
+if ($location8 != "") {
+echo "<a href='play.php?location=".$location8."'>Play ".$song8. "</a>";
+};
+echo "</br>";
+if ($location9 != "") {
+echo "<a href='play.php?location=".$location9."'>Play ".$song9. "</a>";
+};
+echo "</br>";
+if ($location10 != "") {
+echo "<a href='play.php?location=".$location10."'>Play ".$song10. "</a>";
+};
+echo "</br>";
+if ($location11 != "") {
+echo "<a href='play.php?location=".$location11."'>Play ".$song11. "</a>";
+};
+echo "</br>";
+if ($location12 != "") {
+echo "<a href='play.php?location=".$location12."'>Play ".$song12. "</a>";
+};
+echo "</br>";
+if ($location13 != "") {
+echo "<a href='play.php?location=".$location13."'>Play ".$song13. "</a>";
+};
+echo "</br>";
+if ($location14 != "") {
+echo "<a href='play.php?location=".$location14."'>Play ".$song14. "</a>";
+};
+echo "</br>";
+if ($location15 != "") {
+echo "<a href='play.php?location=".$location15."'>Play ".$song15. "</a>";
+};
+echo "</br>";
+if ($location16 != "") {
+echo "<a href='play.php?location=".$location16."'>Play ".$song16. "</a>";
+};
+echo "</br>";
+if ($location17 != "") {
+echo "<a href='play.php?location=".$location17."'>Play ".$song17. "</a>";
+};
+echo "</br>";
+if ($location18 != "") {
+echo "<a href='play.php?location=".$location18."'>Play ".$song18. "</a>";
+};
+echo "</br>";
+if ($location19 != "") {
+echo "<a href='play.php?location=".$location19."'>Play ".$song19. "</a>";
+};
+echo "</br>";
+if ($location20 != "") {
+echo "<a href='play.php?location=".$location20."'>Play ".$song20. "</a>";
+};
+echo "</br>";
+echo "
+</body>
+</html>";
+?>
+
